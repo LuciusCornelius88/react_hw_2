9ea1ac1ebd9e0dec6978816e59e3213a9ae2f6f7
LuciusCornelius88 <volodymir.melnyk@gmail.com>
2023-12-09 10:54:43 +0200

    comment AppPhonebook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppFeedback from 'components/App/App_feedback';
-import AppPhonebook from 'components/App/App_phonebook';
+// import AppPhonebook from 'components/App/App_phonebook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
